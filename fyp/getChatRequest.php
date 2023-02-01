e05fe16e9463ca8ea649be12148ec0c2bf7ce3f3
jtmw1012 <103188536+jtmw1012@users.noreply.github.com>
2023-02-01 14:48:18 +0800

    .

---
+++ fyp/getChatRequest.php
@@ -0,0 +1,37 @@
+<?php
+if (!empty($_POST['userID']) &&
+	!empty($_POST['ticketID'])) 
+{
+	$connection = mysqli_connect("us-cdbr-east-06.cleardb.net", "bbd12ae4b2fcc3", "df9ea7aa", "heroku_80d6ea926f679b3");
+    $userID = $_POST['userID'];
+    $ticketID = $_POST['ticketID'];
+
+    $result = array();
+	$chatsArr = array();
+	
+    if ($connection) {
+		$chatsSQL = "SELECT A.* , U.NAME FROM CHAT A JOIN TICKET B ON A.TICKET=B.ID JOIN USERS U ON A.SENDER=U.ID WHERE B.HOMEOWNER='".$userID."';";
+		$chatsResult = mysqli_query($connection, $chatsSQL);	
+		if (mysqli_num_rows($chatsResult) != 0) {
+			while($chatsRow = mysqli_fetch_array($chatsResult, MYSQLI_ASSOC)){
+				$date = $chatsRow["DATE"];
+				$ticket = $chatsRow["TICKET"];
+				$sender = $chatsRow["SENDER"];
+				$text = $chatsRow["TEXT"];
+				$name = $chatsRow["NAME"];
+				
+				$arr = array("date" => $date, 
+									"ticket" => $ticket, 
+									"name" => $name, 
+									"text" => $text, 
+									);
+									
+				$chatsArr[$sender] = $arr;
+			}
+			$result = array("status" => "success", "message" => "Fetch data successful");
+			$result["chats"] = $chatsArr;
+			
+		} else $result = array("status" => "failed", "message" => "Failed to ticket data");					
+    } else $result = array("status" => "failed", "message" => "Database connection failed");
+} else $result = array("status" => "failed", "message" => "All fields are required");
+echo json_encode($result, JSON_PRETTY_PRINT);
---
+++ fyp/postChatRequest.php
@@ -0,0 +1,45 @@
+<?php
+if( !empty($_POST['userID']) && 
+	!empty($_POST['ticketID']) && 
+	!empty($_POST['text'])){
+		
+    $connection = mysqli_connect("us-cdbr-east-06.cleardb.net", "bbd12ae4b2fcc3", "df9ea7aa", "heroku_80d6ea926f679b3");
+	$userID = $_POST['userID'];
+	$ticketID = $_POST['ticketID'];
+	$text = $_POST['text'];
+
+    if ($connection) {
+		
+		$chatSQL = "INSERT INTO CHAT VALUES (current_timestamp(), '".$ticketID."', '".$userID."', '".$chat."')";
+		if(mysqli_query($connection, $updateUserSQL)){
+			
+				echo "success";
+							
+		} else echo "failed inserting chat";
+		
+		//find homeowner roles ID
+		// $updateUserSQL = "";
+						
+		// $insertSubscribeSQL = "";
+		
+		// if($subStatus == "true"){
+			// $updateUserSQL = "UPDATE HOMEOWNER SET SUBSCRIBE = NULL WHERE ID = '".$userID."'";
+			// $insertSubscribeSQL = "INSERT INTO SUBSCRIBE VALUES ('".$companyID."', '".$userID."', 'NULL', '".$date."')";
+		// }
+		// else{
+			// $updateUserSQL = "UPDATE HOMEOWNER SET SUBSCRIBE = '".$companyID."' WHERE ID = '".$userID."'";
+			// $insertSubscribeSQL = "INSERT INTO SUBSCRIBE VALUES ('".$companyID."', '".$userID."', '".$date."', 'NULL')";
+		// }
+							
+		// if(mysqli_query($connection, $updateUserSQL)){
+			
+			// if(mysqli_query($connection, $insertSubscribeSQL)){
+			
+				// echo "success";
+				
+			// } else echo "failed inserting subscription, subscription failed";
+				
+		// } else echo "failed updating user, edit profile failed";
+		
+    } else echo "Database connection failed";
+} else echo "All fields are required";
