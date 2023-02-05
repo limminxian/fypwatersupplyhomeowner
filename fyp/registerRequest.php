69234230a078385ec13ed896d9e0d96f4359e3e3
jtmw1012 <103188536+jtmw1012@users.noreply.github.com>
2023-02-05 14:52:27 +0800

    .

--- fyp/registerRequest.php
+++ fyp/registerRequest.php
@@ -28,49 +28,58 @@ if( !empty($_POST['name']) &&
 	$homeownerUserID = null;
 	$verificationCode = null;
 	
+	$passwordRegex = '^[^\s]*(?=\S{8,16})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])(?=\S*[\W])[^\s]*$';
+	$emailRegex = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^';
+	$phoneRegex = '^(8|9)\d{7}$';
+	$postalCodeRegex = '^[1-9]\d{5}$';
+	
     if ($connection) {
-		
-		if($password == $retypePassword){
-		
-			$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-			
-					//find homeowner roles ID
-			$getRoleSQL = "SELECT ID FROM ROLE WHERE NAME = 'homeowner'";
-			$roleSQL = mysqli_query($connection, $getRoleSQL);
-			$userRole = mysqli_fetch_row($roleSQL)[0];
-			
-			//Create the user in the db. Status pending until email verified
-			$createUserSQL = "INSERT INTO USERS (NAME, NUMBER, EMAIL, PASSWORD, TYPE, STATUS)
-								VALUES('".$name."',
-										'".$phoneNo."',
-										'".$email."',
-										'".$password."',
-										'".$userRole."',
-										'PENDING')";
-		
-			if(mysqli_query($connection, $createUserSQL)){
-				
-				//Get the user ID to create the homeowner, and generate a verfication code for homeowner
-				$getUserIDSQL = "SELECT MAX(ID) FROM USERS";
-				$homeownerUserID = mysqli_fetch_row(mysqli_query($connection, $getUserIDSQL))[0];
-				
-				//Create the homeowner in the db. 
-				$createHomeownerSQL = "INSERT INTO HOMEOWNER (ID, STREET, BLOCKNO, UNITNO, POSTALCODE, HOUSETYPE, NOOFPEOPLE)
-										VALUES('".$homeownerUserID."',
-												'".$street."',
-												'".$blockNo."',
-												'".$unitNo."',
-												'".$postalCode."',
-												'".$houseType."',
-												'".$householdSize."')";
-					
-				if(mysqli_query($connection, $createHomeownerSQL)){
-					echo "success";
-				} else echo "failed creating homeowner, registration failed";
-					
-			} else echo "failed creating user, registration failed";		
-			
-		} else echo "re-enter passwords do not match";
-		
+		if(preg_match($passwordRegex, $password)){
+			if($password == $retypePassword){
+				if(preg_match($emailRegex, $email)){
+					if(preg_match($phoneRegex, $phoneNo)){
+						if(preg_match($postalCodeRegex, $postalCode)){
+							$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+							
+							//find homeowner roles ID
+							$getRoleSQL = "SELECT ID FROM ROLE WHERE NAME = 'homeowner'";
+							$roleSQL = mysqli_query($connection, $getRoleSQL);
+							$userRole = mysqli_fetch_row($roleSQL)[0];
+							
+							//Create the user in the db. Status pending until email verified
+							$createUserSQL = "INSERT INTO USERS (NAME, NUMBER, EMAIL, PASSWORD, TYPE, STATUS)
+												VALUES('".$name."',
+														'".$phoneNo."',
+														'".$email."',
+														'".$password."',
+														'".$userRole."',
+														'PENDING')";
+						
+							if(mysqli_query($connection, $createUserSQL)){
+								
+								//Get the user ID to create the homeowner, and generate a verfication code for homeowner
+								$getUserIDSQL = "SELECT MAX(ID) FROM USERS";
+								$homeownerUserID = mysqli_fetch_row(mysqli_query($connection, $getUserIDSQL))[0];
+								
+								//Create the homeowner in the db. 
+								$createHomeownerSQL = "INSERT INTO HOMEOWNER (ID, STREET, BLOCKNO, UNITNO, POSTALCODE, HOUSETYPE, NOOFPEOPLE)
+														VALUES('".$homeownerUserID."',
+																'".$street."',
+																'".$blockNo."',
+																'".$unitNo."',
+																'".$postalCode."',
+																'".$houseType."',
+																'".$householdSize."')";
+									
+								if(mysqli_query($connection, $createHomeownerSQL)){
+									echo "success";
+								} else echo "failed creating homeowner, registration failed";
+									
+							} else echo "failed creating user, registration failed";	
+						} else echo "Please insert a valid postal code that includes 6 digits";
+					} else echo "Please insert a valid phone number that starts with 6, 8 or 9 and includes 8 digits";
+				} else echo "Please insert a valid email address";
+			} else echo "re-enter passwords do not match";
+		} else echo "Please provide a password that matched rules above";
     } else echo "Database connection failed";
 } else echo "All fields are required";
