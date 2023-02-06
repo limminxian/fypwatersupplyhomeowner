32be6989d363c34dcce59f9092e27d31ecf61024
jtmw1012 <103188536+jtmw1012@users.noreply.github.com>
2023-02-06 23:14:06 +0800

    x

--- fyp/addPaymentMethodRequest.php
+++ fyp/addPaymentMethodRequest.php
@@ -25,7 +25,7 @@ if( !empty($_POST['userID']) &&
     if ($connection) {
 		$expRegex = '/^[1-3][0-9]\/[0-9]{2}$/';
 		if(preg_match($expRegex, $expiration)){
-			$expMonth = substr($expiration, 2);
+			$expMonth = substr($expiration, 0,2);
 			echo $expMonth;
 			$expYear = substr($expiration, -2);
 			if($expMonth < 32){
