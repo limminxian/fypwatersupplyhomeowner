eb7af88942dbe4831d60dbb8f89627485a1db863
jtmw1012 <103188536+jtmw1012@users.noreply.github.com>
2023-02-06 22:51:17 +0800

    .

--- fyp/addPaymentMethodRequest.php
+++ fyp/addPaymentMethodRequest.php
@@ -25,20 +25,21 @@ if( !empty($_POST['userID']) &&
     if ($connection) {
 		$expRegex = '/^[1-3][0-9]\/[0-9]{2}$/';
 		if(preg_match($expRegex, $expiration)){
-			$expMonth = substr($expiration, 2, 0);
+			$expMonth = substr($expiration, 2);
 			$expYear = substr($expiration, -2);
 			if($expMonth < 32){
-				$cardSQL = "INSERT INTO PAYMENTMETHODS (CUSTOMER, NAME, COUNTRY, ADDRESS, CITY, POSTALCODE, BRAND, EXPMONTH, EXPYEAR, CVC, CARDNO) VALUES ('".$userID."',
-																'".$name."',
-																'".$country."',
-																'".$address."',
-																'".$country."',
-																'".$postalCode."',
-																'".$brand."',																
-																'".$expMonth."',
-																'".$expYear."',
-																'".$cvc."',
-																'".$cardNo."')";
+				$cardSQL = "INSERT INTO PAYMENTMETHODS (CUSTOMER, NAME, COUNTRY, ADDRESS, CITY, POSTALCODE, BRAND, EXPMONTH, EXPYEAR, CVC, CARDNO) 
+							VALUES ('".$userID."',
+									'".$name."',
+									'".$country."',
+									'".$address."',
+									'".$country."',
+									'".$postalCode."',
+									'".$brand."',																
+									'".$expMonth."',
+									'".$expYear."',
+									'".$cvc."',
+									'".$cardNo."')";
 				// $cardSQL = "UPDATE PAYMENTMETHODS SET
 							// CARDNO = '".$cardNo."',
 							// NAME = '".$name."',
