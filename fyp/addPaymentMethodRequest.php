b49d3d3cd0eea4ad6b65c5885180622c153a2941
jtmw1012 <103188536+jtmw1012@users.noreply.github.com>
2023-02-06 23:11:16 +0800

    c

--- fyp/addPaymentMethodRequest.php
+++ fyp/addPaymentMethodRequest.php
@@ -26,6 +26,7 @@ if( !empty($_POST['userID']) &&
 		$expRegex = '/^[1-3][0-9]\/[0-9]{2}$/';
 		if(preg_match($expRegex, $expiration)){
 			$expMonth = substr($expiration, 2);
+			echo $expMonth;
 			$expYear = substr($expiration, -2);
 			if($expMonth < 32){
 				$cardSQL = "INSERT INTO PAYMENTMETHODS (CUSTOMER, NAME, COUNTRY, ADDRESS, CITY, POSTALCODE, BRAND, EXPMONTH, EXPYEAR, CVC, CARDNO) 
