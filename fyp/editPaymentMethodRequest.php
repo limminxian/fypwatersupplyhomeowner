4e9e5d2c4a51020af7ec2a01496520c4454bff1c
jtmw1012 <103188536+jtmw1012@users.noreply.github.com>
2023-02-07 12:22:12 +0800

    .

---
+++ fyp/editPaymentMethodRequest.php
@@ -0,0 +1,61 @@
+<?php
+if( !empty($_POST['cardID']) && 
+	!empty($_POST['cardNo']) && 
+	!empty($_POST['name']) && 
+	!empty($_POST['expiration']) && 
+	!empty($_POST['cvc']) && 
+	!empty($_POST['address']) && 
+	!empty($_POST['country']) && 
+	!empty($_POST['postalCode']) && 
+	!empty($_POST['brand'])){
+		
+    $connection = mysqli_connect("us-cdbr-east-06.cleardb.net", "bbd12ae4b2fcc3", "df9ea7aa", "heroku_80d6ea926f679b3");
+	$cardID = $_POST['cardID'];
+	$cardNo = $_POST['cardNo'];
+	$name = $_POST['name'];
+    $expiration = $_POST['expiration'];
+	$cvc = $_POST['cvc'];
+    $address = $_POST['address'];
+	$country = $_POST['country'];
+    $postalCode = $_POST['postalCode'];
+    $brand = $_POST['brand'];
+	$expMonth = null;
+	$expYear = null;
+	
+    if ($connection) {
+		$expRegex = '/^[1-3][0-9]\/[0-9]{2}$/';
+		if(preg_match($expRegex, $expiration)){
+			$expMonth = substr($expiration, 0,2);
+			$expYear = substr($expiration, -2);
+			if($expMonth < 32){
+				// $cardSQL = "INSERT INTO PAYMENTMETHODS (CUSTOMER, NAME, COUNTRY, ADDRESS, CITY, POSTALCODE, BRAND, EXPMONTH, EXPYEAR, CVC, CARDNO) 
+							// VALUES ('".$userID."',
+									// '".$name."',
+									// '".$country."',
+									// '".$address."',
+									// '".$country."',
+									// '".$postalCode."',
+									// '".$brand."',																
+									// '".$expMonth."',
+									// '".$expYear."',
+									// '".$cvc."',
+									// '".$cardNo."')";
+				$cardSQL = "UPDATE PAYMENTMETHODS SET
+							CARDNO = '".$cardNo."',
+							NAME = '".$name."',
+							EXPMONTH = '".$expMonth."',
+							EXPYEAR = '".$expYear."',
+							CVC = '".$cvc."',
+							ADDRESS = '".$address."',
+							COUNTRY = '".$country."',
+							POSTALCODE = '".$postalCode."',
+							BRAND = '".$brand."',
+							WHERE ID = '".$cardID."'";
+				if(mysqli_query($connection, $cardSQL)){
+					echo "success";
+					
+				} else echo "failed updating payment methods";
+			} else echo "Please enter a valid date";
+		} else echo "Please enter in the correct format";
+    } else echo "Database connection failed";
+} else echo "All fields are required";
---
+++ fyp/getPaymentMethodRequest.php
@@ -0,0 +1,49 @@
+<?php
+if (!empty($_POST['cardID'])) {
+	$connection = mysqli_connect("us-cdbr-east-06.cleardb.net", "bbd12ae4b2fcc3", "df9ea7aa", "heroku_80d6ea926f679b3");
+    $cardID = $_POST['cardID'];
+    $result = array();
+	
+	$cardID = $_POST['cardID'];
+	$cardNo = null;
+	$name = null;
+	$cvc = null;
+    $address = null;
+	$country = null;
+    $postalCode = null;
+    $brand = null;
+	$expMonth = null;
+	$expYear = null;
+	
+    if ($connection) { 
+		$SQL = "SELECT * FROM PAYMENTMETHODS WHERE ID = '".$cardID."'";
+		$Result = mysqli_query($connection, $SQL);
+		
+		if (mysqli_num_rows($Result) != 0) {
+			$Row = mysqli_fetch_assoc($Result);
+			
+			$cardNo = $Row['CARDNO'];
+			$name = $Row['NAME'];
+			$cvc = $Row['CVC'];
+			$address = $Row['ADDRESS'];
+			$country = $Row['COUNTRY'];
+			$postalCode = $Row['POSTALCODE'];
+			$brand = $Row['BRAND'];
+			$expMonth = $Row['EXPMONTH'];
+			$expYear = $Row['EXPYEAR'];
+			
+			$result = array("status" => "success", "message" => "Fetch data successful", 
+						"cardNo" => $cardNo,
+						"name" => $name,
+						"cvc" => $cvc,
+						"address" => $address,
+						"country" => $country,
+						"postalCode" => $postalCode,
+						"brand" => $brand,
+						"expMonth" => $expMonth,
+						"expYear" => $expYear);
+						
+		} else $result = array("status" => "failed", "message" => "Failed to fetch payment method data");				
+    } else $result = array("status" => "failed", "message" => "Database connection failed");
+} else $result = array("status" => "failed", "message" => "All fields are required");
+echo json_encode($result, JSON_PRETTY_PRINT);
