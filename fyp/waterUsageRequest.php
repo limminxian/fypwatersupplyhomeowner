93061c4389cfa6852155bb1258d9cc439f90c0d9
jtmw1012 <103188536+jtmw1012@users.noreply.github.com>
2023-02-08 18:38:16 +0800

    change db

--- fyp/waterUsageRequest.php
+++ fyp/waterUsageRequest.php
@@ -1,6 +1,6 @@
 <?php
 if (!empty($_POST['userID'])) {
-	$connection = mysqli_connect("us-cdbr-east-06.cleardb.net", "bbd12ae4b2fcc3", "df9ea7aa", "heroku_80d6ea926f679b3");
+	$connection = mysqli_connect("us-cdbr-east-06.cleardb.net", "bc292174f8cae7","68916e25", "heroku_80d6ea926f679b3");
 	$userID = $_POST['userID'];
 	$waterArr = array();
 	$result = array();
