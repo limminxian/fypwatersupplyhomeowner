3d3c234ef4034cb125f26fd5581d9b1c61cae552
jtmw1012 <103188536+jtmw1012@users.noreply.github.com>
2023-02-14 02:09:51 +0800

    .

--- fyp/subscribeRequest.php
+++ fyp/subscribeRequest.php
@@ -70,66 +70,71 @@ if( !empty($_POST['userID']) &&
 			//UNSUBSCRIBE
 			$updateUserSQL = "UPDATE HOMEOWNER SET SUBSCRIBE = NULL WHERE ID = '".$userID."'";
 			$insertSubscribeSQL = "INSERT INTO SUBSCRIBE VALUES ('".$companyID."', '".$userID."', 'NULL', '".$date."', '".$maxID."')";
-		}
-		else{
-			//WORKLOAD
-			$csSQL = 	"SELECT U.ID custID, S.WORKLOAD workLoad FROM 
-						USERS U JOIN STAFF S ON U.ID = S.ID 
-						JOIN ROLE R ON U.TYPE = R.ID 
-						WHERE R.NAME = 'customerservice'
-						ORDER BY S.WORKLOAD ASC LIMIT 1";
-			$csResult = mysqli_query($connection, $csSQL);
-			if (mysqli_num_rows($csResult) != 0) {
-				$Row = mysqli_fetch_assoc($csResult);
-				$custID = $Row['custID'];
-				$workLoad = $Row['workLoad'];
+			
+			if(mysqli_query($connection, $updateUserSQL)){
+				if(mysqli_query($connection, $insertSubscribeSQL)){
+					echo "success";
+				} else echo "failed inserting subscription, subscription failed";
+			} else echo "failed updating user, edit profile failed";
+		} else echo "please unsubscribe first";
+		
+		// {
+			////WORKLOAD
+			// $csSQL = 	"SELECT U.ID custID, S.WORKLOAD workLoad FROM 
+						// USERS U JOIN STAFF S ON U.ID = S.ID 
+						// JOIN ROLE R ON U.TYPE = R.ID 
+						// WHERE R.NAME = 'customerservice'
+						// ORDER BY S.WORKLOAD ASC LIMIT 1";
+			// $csResult = mysqli_query($connection, $csSQL);
+			// if (mysqli_num_rows($csResult) != 0) {
+				// $Row = mysqli_fetch_assoc($csResult);
+				// $custID = $Row['custID'];
+				// $workLoad = $Row['workLoad'];
 				
-			} else echo "customer id fetch failed";
+			// } else echo "customer id fetch failed";
 			
-			//SERVICETYPE ID
-			$serviceType = null;
-			$serviceTypeSQL = "SELECT ID FROM SERVICETYPE WHERE NAME = 'installation'";
-			$serviceTypeResult = mysqli_query($connection, $serviceTypeSQL);
-			if(mysqli_num_rows($serviceTypeResult) != 0){
-				$serviceType = mysqli_fetch_row($serviceTypeResult)[0];
-			} else echo "failed to service type id";	
+			////SERVICETYPE ID
+			// $serviceType = null;
+			// $serviceTypeSQL = "SELECT ID FROM SERVICETYPE WHERE NAME = 'installation'";
+			// $serviceTypeResult = mysqli_query($connection, $serviceTypeSQL);
+			// if(mysqli_num_rows($serviceTypeResult) != 0){
+				// $serviceType = mysqli_fetch_row($serviceTypeResult)[0];
+			// } else echo "failed to service type id";	
 			
-			//CREATE TICKET
-			$raiseTicketSQL = "INSERT INTO TICKET ( HOMEOWNER, TYPE, CUSTOMERSERVICE, STATUS, DESCRIPTION, SERVICEDATE) 
-								VALUES ('".$userID."', '".$serviceType."', '".$custID."', 'open','homeowner installation when subscribed','".$date."')";
-			$raiseTicketResult = mysqli_query($connection, $raiseTicketSQL);	
-			if ($raiseTicketResult) {
-			} else echo "failed updating ticket, raise ticket failed";
+			////CREATE TICKET
+			// $raiseTicketSQL = "INSERT INTO TICKET ( HOMEOWNER, TYPE, CUSTOMERSERVICE, STATUS, DESCRIPTION, SERVICEDATE) 
+								// VALUES ('".$userID."', '".$serviceType."', '".$custID."', 'open','homeowner installation when subscribed','".$date."')";
+			// $raiseTicketResult = mysqli_query($connection, $raiseTicketSQL);	
+			// if ($raiseTicketResult) {
+			// } else echo "failed updating ticket, raise ticket failed";
 			
-			//INCREMENT WORKLOAD
-			$workLoad += 1;
-			$increWLSQL = "UPDATE STAFF SET WORKLOAD = '".$workLoad."' WHERE ID = '".$custID."'";
-			$increWLResult = mysqli_query($connection, $increWLSQL);	
-			if ($increWLResult) {
-			} else echo "failed incrementing workload failed";
+			////INCREMENT WORKLOAD
+			// $workLoad += 1;
+			// $increWLSQL = "UPDATE STAFF SET WORKLOAD = '".$workLoad."' WHERE ID = '".$custID."'";
+			// $increWLResult = mysqli_query($connection, $increWLSQL);	
+			// if ($increWLResult) {
+			// } else echo "failed incrementing workload failed";
 			
-			//TICKET ID
-			$maxID = null;
-			$maxIDSQL = "SELECT MAX(ID) FROM TICKET";
-			$maxIDResult = mysqli_query($connection, $maxIDSQL);
-			if(mysqli_num_rows($maxIDResult) != 0){
-				$maxID = mysqli_fetch_row($maxIDResult)[0];
-			} else echo "failed to get max ticket id";	
+			////TICKET ID
+			// $maxID = null;
+			// $maxIDSQL = "SELECT MAX(ID) FROM TICKET";
+			// $maxIDResult = mysqli_query($connection, $maxIDSQL);
+			// if(mysqli_num_rows($maxIDResult) != 0){
+				// $maxID = mysqli_fetch_row($maxIDResult)[0];
+			// } else echo "failed to get max ticket id";	
 			
-			//SUBSCRIBE
-			$updateUserSQL = "UPDATE HOMEOWNER SET SUBSCRIBE = '".$companyID."' WHERE ID = '".$userID."'";
-			$insertSubscribeSQL = "INSERT INTO SUBSCRIBE VALUES ('".$companyID."', '".$userID."', '".$date."', 'NULL', '".$maxID."')";
-		}
+			////SUBSCRIBE
+			// $updateUserSQL = "UPDATE HOMEOWNER SET SUBSCRIBE = '".$companyID."' WHERE ID = '".$userID."'";
+			// $insertSubscribeSQL = "INSERT INTO SUBSCRIBE VALUES ('".$companyID."', '".$userID."', '".$date."', 'NULL', '".$maxID."')";
+			
+			// if(mysqli_query($connection, $updateUserSQL)){
+				// if(mysqli_query($connection, $insertSubscribeSQL)){
+					// echo "success";
+				// } else echo "failed inserting subscription, subscription failed";
+			// } else echo "failed updating user, edit profile failed";
+		// }
 							
-		if(mysqli_query($connection, $updateUserSQL)){
-			
-			if(mysqli_query($connection, $insertSubscribeSQL)){
-			
-				echo "success";
-				
-			} else echo "failed inserting subscription, subscription failed";
-				
-		} else echo "failed updating user, edit profile failed";
+		
 		
     } else echo "Database connection failed";
 } else echo "All fields are required";
