7dea1e202a22a2773b1b3ea573156ae2b01fb685
jtmw1012 <103188536+jtmw1012@users.noreply.github.com>
2023-02-15 23:55:26 +0800

    .

---
+++ fyp/getSubStatus.php
@@ -0,0 +1,17 @@
+<?php include_once 'config.php';
+if (!empty($_POST['userID'])) {
+	$connection = getDB();
+	
+    $userID = $_POST['userID'];
+	$type = $_POST['type'];
+	$description = $_POST['description'];
+
+    if ($connection) {
+		$SQL = "SELECT SUBSCRIBE FROM SERVICETYPE WHERE ID = '".$userID."'";
+		$Result = mysqli_query($connection, $SQL);
+		$sub = mysqli_fetch_row($Result)[0];
+		if($sub != null){
+			echo "true";
+		} else echo "false";
+    } else echo "Database connection failed";
+} else echo "All fields are required";
