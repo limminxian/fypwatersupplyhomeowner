05c2556e56777b279bb173277db4bad91484de62
jtmw1012 <103188536+jtmw1012@users.noreply.github.com>
2023-02-15 14:22:33 +0800

    .

--- fyp/removePaymentMethodRequest.php
+++ fyp/removePaymentMethodRequest.php
@@ -1,5 +1,4 @@
 <?php
-if(!empty($_POST['cardID'])){
 		
     $connection = mysqli_connect("us-cdbr-east-06.cleardb.net", "bc292174f8cae7", "68916e25", "heroku_a43ceec7a5c075b");
 	$cardID = $_POST['cardID'];
@@ -11,4 +10,3 @@ if(!empty($_POST['cardID'])){
 					
 		} else echo "failed updating payment methods";
     } else echo "Database connection failed";
-} else echo "All fields are required".$cardID;
