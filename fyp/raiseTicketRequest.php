0fa5069f9beab6aa456ba6b6b5b1ecb294bc7166
jtmw1012 <103188536+jtmw1012@users.noreply.github.com>
2023-02-18 02:04:17 +0800

    .

--- fyp/raiseTicketRequest.php
+++ fyp/raiseTicketRequest.php
@@ -15,17 +15,25 @@ if (!empty($_POST['userID'])
 		$Result = mysqli_query($connection, $SQL);
 		$typeID = mysqli_fetch_row($Result)[0];
 		
+		//Get subbed company
+		$SQL = "SELECT SUBSCRIBE FROM HOMEOWNER WHERE ID = '".$userID."'";
+		$Result = mysqli_query($connection, $SQL);
+		$subbedID = mysqli_fetch_row($Result)[0];
+		
 		//get least workload customerservice staff
 		$custID = null;
-		$csSQL = 	"SELECT U.ID FROM 
-					USERS U JOIN STAFF S ON U.ID = S.ID 
-					JOIN ROLE R ON U.TYPE = R.ID 
-					WHERE R.NAME = 'customerservice'
-					ORDER BY S.WORKLOAD ASC LIMIT 1";
-		$csResult = mysqli_query($connection, $csSQL);
-		if(mysqli_num_rows($csResult) != 0){
-			$custID = mysqli_fetch_row($csResult)[0];
-		} else echo "customer id fetch failed";
+		if($subbedID != null){
+			$csSQL = 	"SELECT U.ID FROM 
+						USERS U JOIN STAFF S ON U.ID = S.ID 
+						JOIN ROLE R ON U.TYPE = R.ID 
+						WHERE R.NAME = 'customerservice'
+						AND S.COMPANY = '".$subbedID."'
+						ORDER BY S.WORKLOAD ASC LIMIT 1";
+			$csResult = mysqli_query($connection, $csSQL);
+			if(mysqli_num_rows($csResult) != 0){
+				$custID = mysqli_fetch_row($csResult)[0];
+			} else echo "customer id fetch failed";
+		} else echo "user is not subbed to any company";
 		
 		//services and rates 
 		$raiseTicketSQL = "INSERT INTO TICKET ( HOMEOWNER, TYPE, CUSTOMERSERVICE, STATUS, DESCRIPTION, SERVICEDATE) 
